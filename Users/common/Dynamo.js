f9e76618e4350c06111907f74aecab98ccbc7e6e
YUBYEONGGUK <godkor200@gmail.com>
2020-12-03 23:08:25 +0900

    Member sign-in implementation completed, Additionally password hashing is required

--- Users/common/Dynamo.js
+++ Users/common/Dynamo.js
@@ -1,31 +1,113 @@
 const AWS = require("aws-sdk");
 const documentClient = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
+const crypto = require("crypto");
 
 AWS.config.update({
   region: "ap-northeast-2",
   endpoint: "http://dynamodb.ap-northeast-2.amazonaws.com",
 });
+
+function hashPassword(password) {
+  let salt = "random string";
+  let shasum = crypto.createHash("sha1");
+  shasum.update(password + salt);
+  let result = shasum.digest("hex");
+  console.log("🚀 ~ file: Dynamo.js ~ line 16 ~ hashPassword ~ result", result);
+  return result;
+}
+
 const Dynamo = {
-  async write(userData, TableName) {
-    let putParams = {
-      TableName: "Users",
-      Item: {
-        userId: Math.floor(Math.random() * 100000),
-        user_name: userData.user_name,
-        password: userData.password,
-        createdAt: `${new Date().getFullYear()}년 ${new Date().getMonth()}월 ${new Date().getDate()}일 ${new Date().getHours()}시 ${new Date().getMinutes()}분 ${new Date().getSeconds()}초`,
+  //Users 삭제
+  async _delete(userId) {
+    let deleteParams = {
+      Key: {
+        userId: userId,
       },
+      TableName: "Users",
     };
-    console.log(
-      "🚀 ~ file: Dynamo.js ~ line 19 ~ write ~ putParams",
-      putParams
-    );
-    let data = await documentClient.put(putParams).promise();
-    console.log("🚀 ~ file: Dynamo.js ~ line 24 ~ write ~ data", data);
-    if (!data) {
-      throw Error(`There was an error put in the data from ${TableName}`);
-    }
+    return await documentClient.delete(deleteParams).promise();
+  },
+  //Users 수정
+  async _update(userData, userId) {
+    const { nickName, password, user_name } = userData;
+    let updateParams = {
+      Key: {
+        userId: userId,
+      },
+      TableName: "Users",
+      ConditionExpression: "attribute_exists(userId)",
+      UpdateExpression:
+        "SET nickName = :nickName, password = :password, user_name = :user_name updateAt = :updateAt",
+      ExpressionAttributeValues: {
+        ":nickName": nickName,
+        ":password": hashPassword(password),
+        ":user_name": user_name,
+        ":updateAt": new Date().toISOString(),
+      },
+      ReturnValues: "ALL_NEW",
+    };
+    let data = await documentClient.update(updateParams).promise();
     return data;
+  },
+  //Users 로그인
+  async _signIn(userData) {
+    let paramsScan = {
+      TableName: "Users",
+    };
+    let data = await documentClient.scan(paramsScan).promise();
+    let seached = { data }.data.Items;
+    const { email, password } = userData;
+
+    for (let i = 0; i < seached.length; i++) {
+      if (
+        seached[i].email === email &&
+        seached[i].password === hashPassword(password)
+      ) {
+        console.log("There's the ID and password Here.");
+        return seached[i];
+      } else {
+        console.log("There's NO !!!! the ID and password Here.");
+      }
+    }
+  },
+  //Users 회원가입
+  async _signUp(userData, TableName) {
+    let idScan = {
+      TableName: "Users",
+    };
+    let data = await documentClient.scan(idScan).promise();
+    let seached = { data }.data.Items;
+    const { email, password } = userData;
+    console.log(
+      "🚀 ~ file: Dynamo.js ~ line 82 ~ _signUp ~ password",
+      password
+    );
+
+    for (let i = 0; i < seached.length; i++) {
+      if (
+        seached[i].email === email &&
+        seached[i].password === hashPassword(password)
+      ) {
+        // 중복아이디인 경우
+        return;
+      } else {
+        let putParams = {
+          TableName: "Users",
+          Item: {
+            userId: uuidv4(),
+            email: email,
+            password: password,
+            createdAt: new Date().toISOString(),
+          },
+        };
+        let data = await documentClient.put(putParams).promise();
+        if (!data) {
+          throw Error(`There was an error put in the data from ${TableName}`);
+        }
+        return data;
+      }
+    }
   },
 };
 module.exports = Dynamo;
--- Users/endpoint/signin.js
+++ Users/endpoint/signin.js
@@ -1,6 +1,5 @@
 const Response = require("../common/API_Response");
 const Dynamo = require("../common/Dynamo");
-const TableName = { TableName: "Users" };
 
 exports.handler = async (event) => {
   event = JSON.parse(event.body);
@@ -14,17 +13,19 @@ exports.handler = async (event) => {
     Response._400({
       message: "Post must have a email and password and they must not be empty",
     });
+    return null;
   }
   let userData = event;
 
-  const data = await Dynamo._signUp(userData, TableName).catch((err) => {
+  const data = await Dynamo._signIn(userData).catch((err) => {
     console.log("error in Dynamo userData post", err);
     return null;
   });
 
   if (data) {
     //return data
-    return Response._200({ message: "It's a success. userData" });
+    return Response._200({ message: "It's a success. signin", data });
+  } else {
+    return Response._400({ message: "Failed signin." });
   }
-  return Response._400({ message: "already used." });
 };
