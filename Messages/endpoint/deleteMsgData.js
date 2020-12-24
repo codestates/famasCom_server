5c23faa9613bc43fd754c66667ce68ca9f283a7b
YUBYEONGGUK <godkor200@gmail.com>
2020-12-24 09:50:54 +0900

    fix issue

--- Messages/endpoint/deleteMsgData.js
+++ Messages/endpoint/deleteMsgData.js
@@ -1,8 +1,12 @@
 const Response = require("../common/API_Response");
 const Dynamo = require("../common/Dynamo");
 exports.handler = async (event) => {
-  const userId = event.requestContext.authorizer.principalId;
+  //const userId = event.requestContext.authorizer.principalId;
   const msgId = event.pathParameters.msgId;
+  console.log(
+    "🚀 ~ file: deleteMsgData.js ~ line 6 ~ exports.handler= ~ msgId",
+    msgId
+  );
 
   let data = await Dynamo._delete(msgId).catch((err) => {
     console.log("error in Dynamo msgData update", err);
@@ -10,7 +14,7 @@ exports.handler = async (event) => {
   });
   if (data) {
     //return data
-    return Response._200({ message: "The message has been deleted.", userId });
+    return Response._200({ message: "The message has been deleted." });
   }
   return Response._400({ message: "Failed to delete message." });
 };
