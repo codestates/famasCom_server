97ef783422a3dcf9d94f21ba6d7c4df902c6f22c
YUBYEONGGUK <godkor200@gmail.com>
2021-01-02 18:47:47 +0900

    Setting environment variables

--- hello.js
+++ hello.js
@@ -1,17 +1,14 @@
-const Dynamo = require("./Users/common/Dynamo");
 const Response = require("./Users/common/API_Response");
 
 exports.handler = async (event) => {
-  let userId = event.requestContext.authorizer.principalId;
-  let data = await Dynamo._search(userId).catch((err) => {
-    console.log("error in Dynamo Get username", err);
-    return null;
-  });
-  if (data) {
-    return Response._200({
-      message: "successfully rendered",
-      username: data.Item.user_name,
-      userNickname: data.Item.nickName,
-    });
+  let origin = event.headers.origin;
+  if (
+    origin === "http://localhost:3000" ||
+    "https://dev.d3ozv5r487dny8.amplifyapp.com" ||
+    "https://dev.myfamas.com"
+  ) {
+    return Response._200({ message: process.env.KAKAO_SECRET });
+  } else {
+    return Response._400({ message: failed });
   }
 };
